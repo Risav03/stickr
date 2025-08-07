6ffbd4eaeaef3e556ad841cbe552b5c55d62b035
Risavdeb Patra <needleweed03@gmail.com>
2025-08-07 16:07:56 +0530

    not using isInMiniApp thing

--- lib/fetchWrapper.ts
+++ lib/fetchWrapper.ts
@@ -1,21 +1,8 @@
-import { useIsInMiniApp } from "@coinbase/onchainkit/minikit";
 import sdk from "@farcaster/miniapp-sdk";
-import { useQuery } from "@tanstack/react-query";
 
-export async function fetchWrapper(endpoint:string, options: RequestInit = {}) {
-    const {isInMiniApp} = useIsInMiniApp();
-
-    return useQuery({
-    queryKey: ["useQuickAuth", isInMiniApp],
-    queryFn: async () => {
-      // If we're in a mini app context, all we have to do to make an authenticated
-      // request is to use `sdk.quickAuth.fetch`. This will automatically include the
-      // necessary `Authorization` header for the backend to verify.
-      const result = await sdk.quickAuth.fetch(endpoint, options);
-
-      const jsonResult = await result.json();
-      return jsonResult
-    },
-    enabled: isInMiniApp,
-  });
+export async function fetchWrapper(endpoint: string, options: RequestInit = {}) {
+  // Make an authenticated request via Farcaster miniapp SDK
+  const result = await sdk.quickAuth.fetch(endpoint, options);
+  const jsonResult = await result.json();
+  return jsonResult;
 }

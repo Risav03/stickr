07286c97611930b85c65a8c7a9ce80129ff99ad9
Risav03 <needleweed03@gmail.com>
2025-08-08 21:11:36 +0530

    col update

--- components/UI/Background.tsx
+++ components/UI/Background.tsx
@@ -1,9 +1,9 @@
 export default function Background() {
   return (
-    <div className="fixed z-[-1] inset-0 overflow-hidden ">
-      <div className="absolute bg-blue-600/90 w-[300px] aspect-square rounded-full blur-[250px] -top-72 -left-32"></div>
-      <div className="absolute bg-blue-500/60 w-[300px] aspect-square rounded-full blur-[250px] -bottom-48 -left-80"></div>
-<div className="absolute bg-blue-700/80 w-[250px] aspect-square rounded-full blur-[350px] bottom-60 -right-80"></div>
+    <div className="fixed z-[-1] inset-0 overflow-hidden bg-black">
+      <div className="absolute bg-blue-400 w-[300px] aspect-square rounded-full blur-[220px] -top-72 -left-32"></div>
+      <div className="absolute bg-blue-500/50 w-[400px] aspect-square rounded-full blur-[200px] -bottom-32 -left-48"></div>
+<div className="absolute bg-blue-700/80 w-[300px] aspect-square rounded-full blur-[250px] bottom-60 -right-80"></div>
     </div>
   );
 }
